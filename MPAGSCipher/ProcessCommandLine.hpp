77dc1c9366911aaad4714c798fd34a1c5fa49f35
Thomas Latham <T.Latham@warwick.ac.uk>
2016-11-02 19:52:18 +0000

    Declare the ProgramSettings struct

--- MPAGSCipher/ProcessCommandLine.hpp
+++ MPAGSCipher/ProcessCommandLine.hpp
@@ -4,6 +4,15 @@
 #include <string>
 #include <vector>
 
+struct ProgramSettings {
+  bool helpRequested;     ///< Indicates the presence of the help flag in the arguments
+  bool versionRequested;  ///< Indicates the presence of the version flag in the arguments
+  std::string inputFile;  ///< Name of the input file
+  std::string outputFile; ///< Name of the output file
+  std::string cipherKey;  ///< Key to be used in encrypting/decrypting routine
+  bool encrypt;           ///< Flag indicating whether the cipher should encrypt (true) or decrypt (false)
+};
+
 bool processCommandLine(const std::vector<std::string>& args,
                         bool& helpRequested,
                         bool& versionRequested,
