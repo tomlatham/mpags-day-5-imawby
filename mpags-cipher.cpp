fd7a0b3c25665feadedb44bf895bf2aa528a1d1b
Thomas Latham <T.Latham@warwick.ac.uk>
2017-10-28 11:45:43 +0100

    Improve the comments regarding the checks prior to converting the key from a string to an unsigned long

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -109,12 +109,17 @@ int main(int argc, char* argv[])
 	// We default to having a key of 0, i.e. no encryption, if no key was provided on the command line
 	size_t caesarKey {0};
 	if ( ! settings.cipherKey.empty() ) {
-	  // Before doing the conversion we should check that the string contains a valid positive integer.
-	  // Here we do that by looping through each character and checking that it is a digit.
-	  // (Since the conversion function will throw an exception if the string does
-	  // not represent a valid integer, we could have checked for and handled
-	  // that instead but we do not cover exceptions at all in this course - they
-	  // are a very complex area of C++ that could take an entire course on their own!)
+	  // Before doing the conversion we should check that the string contains a
+	  // valid positive integer.
+	  // Here we do that by looping through each character and checking that it
+	  // is a digit. What is rather hard to check is whether the number is too
+	  // large to be represented by an unsigned long, so we've omitted that for
+	  // the time being.
+	  // (Since the conversion function std::stoul will throw an exception if the
+	  // string does not represent a valid unsigned long, we could check for and
+	  // handled that instead but we only cover exceptions very briefly on the
+	  // final day of this course - they are a very complex area of C++ that
+	  // could take an entire course on their own!)
 	  for ( const auto& elem : settings.cipherKey ) {
 	    if ( ! std::isdigit(elem) ) {
 	      std::cerr << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
